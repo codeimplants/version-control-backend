ae103c1720f24def1f183214871c73794773cc8c
Aniket Potdar <aniketpanditpotdar@gmail.com>
2026-02-17 23:33:24 +0530

    Updated cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,14 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://versioncontrol.codeimplants.com',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
